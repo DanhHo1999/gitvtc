4a3cd5a8b6654d98e9ab60bc08c342cf2fc35224
Danh Ho <ho.thanhdanh9400@gmail.com>
2022-05-20 11:36:17 +0700

    reset

--- 02 OOP/19-05 001 PTBac2/Program.cs
+++ 02 OOP/19-05 001 PTBac2/Program.cs
@@ -10,6 +10,12 @@
 
             PTBac2 pt2 = new PTBac2(3, 9, 0);
             pt2.GiaiPhuongTrinh();
+
+            pt1 = new PTBac2(6, 9, 0);
+            pt1.GiaiPhuongTrinh();
+
+            pt1=new PTBac2(5, 2, 0);
+            pt1.GiaiPhuongTrinh();
         }
     }
 }
