0751dc6f5e3561c3c642ac7ace257ee958901e38
Danh Ho <ho.thanhdanh9400@gmail.com>
2022-05-20 11:09:21 +0700

    reset

--- 02 OOP/19-05 001 PTBac2/Program.cs
+++ 02 OOP/19-05 001 PTBac2/Program.cs
@@ -2,25 +2,14 @@
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            while (true)
-            {
+            PTBac2 pt1 = new PTBac2(1, 9, 0);
+            pt1.GiaiPhuongTrinh();
 
-                Console.WriteLine("Ax + Bx + C = 0");
-                Console.Write("A = "); int a = Convert.ToInt32(Console.ReadLine());
-                Console.Write("B = "); int b = Convert.ToInt32(Console.ReadLine());
-                Console.Write("C = "); int c = Convert.ToInt32(Console.ReadLine());
-                int del = b * b - 4 * a * c;
-                if (del < 0) Console.WriteLine("PTVN");
-                else if (del == 0) Console.WriteLine("x1 = x2 = " + ((-b - Math.Sqrt(del)) / 2 / a));
-                else
-                {
-                    Console.WriteLine("x1 = " + ((-b - Math.Sqrt(del)) / 2 / a));
-                    Console.WriteLine("x2 = " + ((-b + Math.Sqrt(del)) / 2 / a));
-                }
-                Console.WriteLine();
-            }
+            PTBac2 pt2 = new PTBac2(3, 9, 0);
+            pt2.GiaiPhuongTrinh();
         }
     }
 }
