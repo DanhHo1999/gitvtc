60cc682a0924868dfe72d1608c56c84a1041584c
Danh Ho <ho.thanhdanh9400@gmail.com>
2022-05-21 10:24:26 +0700

    reset

---
+++ 02 OOP/21-05 003 Inheritance/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AAA {
+    class Program {
+        public class A {
+            private int id = 5;
+            public int getID() { 
+                return id;
+            }
+
+        }
+        public class B : A {
+            
+        }
+
+        static void Main(string[] args) { 
+
+            A a = new A();
+            Console.WriteLine(a.getID());
+
+            B b = new B();
+            Console.WriteLine(b.getID());
+
+        }
+    }
+}
