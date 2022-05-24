9bedf9b60ce33cc3f0015ece6a349a615969727a
Danh Ho <ho.thanhdanh9400@gmail.com>
2022-05-24 10:47:52 +0700

    24 - 5

---
+++ 02 OOP/24-05 004 LTHDT Bai 1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AAA {
+    class Program {
+        public class ToaDo
+        {
+            private int x;
+            private int y;
+            public ToaDo() { }
+            public ToaDo(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public int getX() { return x; }
+            public int getY() { return y; }
+            public void setX(int x) { this.x = x; }
+            public void setY(int y) { this.y = y; }
+            public String toString() {
+                String str = "X : " + x + ", Y : " + y;
+                return str;
+            }
+
+        }
+        static void Main(string[] args) { 
+            ToaDo toaDo = new ToaDo(3,8);
+            Console.WriteLine(toaDo.toString());
+        }
+    }
+}
---
+++ 02 OOP/24-05 005 LTHDT Bai 2/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace AAA
+{
+    class Program
+    {
+        public class SinhVien {
+            private int id;
+            private string name;
+            private float TH, LT;
+            public SinhVien(int _id, string _name) { 
+                id = _id;
+                name = _name;
+            }
+            public SinhVien(int _id, string _name,float _TH, float _LT)
+            {
+                id = _id;
+                name= _name;
+                TH = _TH;
+                LT = _LT;
+            }
+            public void SetID(int _id) {
+                id = _id;    
+            }
+            public void SetName(string name) {
+                this.name = name;
+            }
+            public string GetName() { 
+                return this.name;
+            }
+            public int GetID() { 
+                return id;
+            }
+            public void SetTH(float TH) { 
+                this.TH= TH;
+            }
+            public void SetLT(float LT) { 
+                this.LT= LT;
+            }
+            public float GetLT() { 
+                return LT;
+            }
+            public float GetTH() {
+                return TH;
+            }
+            public String ToString() {
+                String str = "";
+                return str;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
