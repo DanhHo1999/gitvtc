4ce66481325b39ebb3f614b5bbca32ddf48d7893
Danh Ho <ho.thanhdanh9400@gmail.com>
2022-05-25 14:22:15 +0700

    25 - 5

--- 02 OOP/24-05 008 LTHDT Bai 5/HangThucPham.cs
+++ 02 OOP/24-05 008 LTHDT Bai 5/HangThucPham.cs
@@ -11,19 +11,110 @@ namespace _24_05_008_LTHDT_Bai_5
         int id = -1;
         string name = "xxx";
         int price;
+        DateOnly nsx;
+        DateOnly hsd;
+        public static DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+        public static void SetToday(int _year,int _month,int _day) {
+            HangThucPham.today = new DateOnly(_year, _month, _day);
+        }
+
+        public HangThucPham(int _id, string _name, int _price,DateOnly _nsx, DateOnly _hsd) { 
+            SetID(_id);
+            SetName(_name);
+            SetPrice(_price);
+            SetNsx(_nsx);
+            SetHsd(_hsd);
+        }
+        public HangThucPham(int _id, string _name, int _price)
+        {
+            SetID(_id);
+            SetName(_name);
+            SetPrice(_price);
+        }
+
         public void SetID(int _id) {
             if (id == -1) id = _id;
         }
-        public int getId() {
+        public int GetId() {
             return id;
             
         }
-        public void setName(string _name) {
+        public void SetName(string _name) {
             if (_name == "") { name = "xxx"; return; }
             name = _name;
         }
-        public string getName() { 
+        public string GetName() { 
             return name;
+        }
+        public void SetPrice(int _price) { 
+            price= _price;
+        }
+        public int GetPrice() { return price; }
+        public void SetNsx(int _year,int _month, int _day)
+        {
+            DateOnly _nsx = new DateOnly(_year, _month, _day);
+            if (_nsx.CompareTo(DateOnly.FromDateTime(DateTime.Now)) < 0)
+            {
+                nsx = _nsx;
+            }
+            else
+            {
+                nsx = DateOnly.FromDateTime(DateTime.Now);
+            }
+        }
+
+        public void SetNsx(DateOnly _nsx)
+        {
+            if (_nsx.CompareTo(DateOnly.FromDateTime(DateTime.Now)) < 0)
+            {
+                nsx = _nsx;
+            }
+            else
+            {
+                nsx = DateOnly.FromDateTime(DateTime.Now);
+            }
+        }
+        public DateOnly GetNsx() {
+            return nsx;
+        }
+        public void SetHsd(int _year, int _month, int _day)
+        {
+            DateOnly _hsd = new DateOnly(_year, _month, _day);
+            if (_hsd.CompareTo(nsx) > 0)
+            {
+                hsd = _hsd;
+            }
+            else
+            {
+                hsd = nsx;
+            }
+        }
+
+        public void SetHsd(DateOnly _hsd)
+        {
+            if (_hsd.CompareTo(nsx) > 0)
+            {
+                hsd = _hsd;
+            }
+            else
+            {
+                hsd = nsx;
+            }
+        }
+        public bool checkHsd()
+        {
+            
+            if (hsd.CompareTo(HangThucPham.today) >= 0) 
+                return true;
+            else 
+                return false;
+        }
+
+        public string ToString()
+        {
+            String str = String.Format("{0,4:000} | {1,8} | {2,15:#,###00.00 VND} | {3,10:dd/MM/yyyy} | {4,10:dd/MM/yyyy} | {5}",
+                id, name, price, nsx, hsd, checkHsd() ? "Chua het han" : "Da het han");
+            return str;
         }
     }
 }
--- 02 OOP/24-05 008 LTHDT Bai 5/Program.cs
+++ 02 OOP/24-05 008 LTHDT Bai 5/Program.cs
@@ -1,10 +1,36 @@
 ﻿namespace _24_05_008_LTHDT_Bai_5 {
     internal class Program {
         static void Main(string[] args) {
+            HangThucPham.SetToday(2018, 6, 1);
+            Console.WriteLine(String.Format("{0,4} | {1,8} | {2,15} | {3,10} | {4,10} | {5}\n",
+                "ID", "Name", "Price", "NSX", "HSD", "Kiem Tra"));
+            
 
-            DateTime now=DateTime.Now;
-            DateTime now2 = DateTime.Now;
-            DateTime t = new DateTime();
+
+
+            HangThucPham gao = new HangThucPham(1,"Gao",100000);
+            gao.SetNsx(2018, 7, 10);
+            gao.SetHsd(new DateOnly(2018, 7, 10));
+            Console.WriteLine(gao.ToString());
+
+
+
+
+            HangThucPham mi = new HangThucPham(2, "Mi", 5000,
+                new DateOnly(2018,3,1),
+                new DateOnly(2018,9,1)
+                );
+            Console.WriteLine(mi.ToString());
+
+
+
+
+            HangThucPham nuoc = new HangThucPham(3,"Nuoc",9999);
+            nuoc.SetID(5);
+            nuoc.SetPrice(10000);
+            nuoc.SetNsx(2017, 3, 1);
+            nuoc.SetHsd(2018, 3, 1);
+            Console.WriteLine(nuoc.ToString());
 
         }
     }
