359d6c4f6f9d6f07580b0c6238070d64ea9645a7
Danh Ho <ho.thanhdanh9400@gmail.com>
2022-05-30 15:18:41 +0700

    30 - 5

--- 02 OOP/30-05 014 LTHDT Bai 9/DanhSachCongNhan.cs
+++ 02 OOP/30-05 014 LTHDT Bai 9/DanhSachCongNhan.cs
@@ -8,10 +8,47 @@ namespace _30_05_014_LTHDT_Bai_9
 {
     internal class DanhSachCongNhan
     {
+        int currentSize = 0;
         CongNhan[] list;
         public DanhSachCongNhan(int _size) { 
             list = new CongNhan[_size];
-            
+        }
+        public void Add(CongNhan _congNhan)
+        {
+            if (currentSize <= list.Length && checkExistedID(_congNhan.getMaCN()))
+            {
+                list[currentSize] = _congNhan;
+                currentSize++;
+            }
+        }
+        public bool checkExistedID(int _id) {
+            for (int i = 0; i < currentSize; i++) {
+                if (_id == list[i].getMaCN()) return false;
+            }
+            return true;
+        }
+        public void ShowAll() {
+            for (int i = 0; i < currentSize; i++)
+            {
+                Console.WriteLine(list[i].ToString());
+            }
+        }
+        public int SoLuongNhanVien() {
+            return currentSize;
+        }
+        public void SwitchCongNhan(ref CongNhan cn1, ref CongNhan cn2) {
+            CongNhan tam = cn1;
+            cn1 = cn2;
+            cn2 = tam;
+        }
+        public DanhSachCongNhan SortQuantityDescending() {
+            for (int i = 0; i < currentSize - 1; i++) {
+                for (int j = i + 1; j < currentSize; j++)
+                {
+                    if (list[i].getSoSP() < list[j].getSoSP()) SwitchCongNhan(ref list[i], ref list[j]);
+                }
+            }
+            return this;
         }
     }
 }
--- 02 OOP/30-05 014 LTHDT Bai 9/Program.cs
+++ 02 OOP/30-05 014 LTHDT Bai 9/Program.cs
@@ -2,12 +2,17 @@
     internal class Program {
         static void Main(string[] args) {
             CongNhan a = new CongNhan(1,"Ho","Danh",100);
-            Console.WriteLine(a.ToString());
 
 
-            CongNhan[] list = new CongNhan[5];
-            list[0] = a;
-            Console.WriteLine(list.Length);
+            DanhSachCongNhan danhSach = new DanhSachCongNhan(10);
+            danhSach.Add(a);
+            danhSach.Add(new CongNhan(2,"BBB","BB",500));
+            danhSach.Add(new CongNhan(3,"AAC","CCA",300));
+
+            danhSach.ShowAll();
+            Console.WriteLine();
+
+            danhSach.SortQuantityDescending().ShowAll();
         }
     }
 }
