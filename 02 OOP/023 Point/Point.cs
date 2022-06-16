be4179cdfaace5082d833de7620552771fa26741
Danh Ho <ho.thanhdanh9400@gmail.com>
2022-06-16 14:16:38 +0700

    16/6

--- 02 OOP/023 Point/Point.cs
+++ 02 OOP/023 Point/Point.cs
@@ -6,13 +6,32 @@ using System.Threading.Tasks;
 
 namespace _023_Point
 {
-    internal class Point
+    internal class Point:IComparable<Point>
     {
         int x, y;
+
         public int getX() { return x; }
         public int getY() { return y; }
         public void setX(int x) { this.x = x; }
         public void setY(int y) { this.y = y; }
+
+        public int CompareTo(Point point)
+        {
+            if (this.x > point.x)
+                return 1;
+            else if (this.x < point.x)
+                return -1;
+            else
+            {
+                if (this.y > point.y)
+                    return 1;
+                else if (this.y < point.y)
+                    return -1;
+                else
+                    return 0;
+            }
+        }
+
         public Point(int x, int y) { this.x = x; this.y = y; }
 
     }
--- 02 OOP/023 Point/Program.cs
+++ 02 OOP/023 Point/Program.cs
@@ -4,8 +4,18 @@
         public static void Main(string[] args) { 
             List<Point> points = new List<Point>();
             points.Add(new Point(1,1));
-            points.Add(new Point(2,1));
-            Console.WriteLine(points[0].Equals(points[1]));
+            points.Add(new Point(1,4));
+            points.Add(new Point(1,5));
+            points.Add(new Point(1,2));
+            points.Add(new Point(1,1));
+
+            points.Sort();
+
+            foreach (Point p in points)
+            {
+                Console.WriteLine(p.getX() + "," + p.getY());
+            }
+
         }
     }
 }
