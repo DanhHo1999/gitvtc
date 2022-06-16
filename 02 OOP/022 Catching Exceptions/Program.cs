35f3ada672af19397327bd6bd03ed6750061f937
Danh Ho <ho.thanhdanh9400@gmail.com>
2022-06-16 12:56:21 +0700

    16/6

--- 02 OOP/022 Catching Exceptions/Program.cs
+++ 02 OOP/022 Catching Exceptions/Program.cs
@@ -1,17 +1,4 @@
-﻿//int n = 0;
-//do
-//{
-//    Console.Write("N = ");
-//    try
-//    {
-//        n = Convert.ToInt32(Console.ReadLine());
-//        if (n <= 50 && n >= 10) break;  //Break loop
-//    }
-//    catch (Exception) { }
-//    Console.WriteLine("Integer Required (10-50)");
-//} while (true);
-//Console.WriteLine("Good");
-int n = 0;
+﻿int n = 0;
 do
 {
     Console.Write("N = ");
