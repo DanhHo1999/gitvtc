8f76fe32f31015bea3db973d9ad725fe2a098f29
Danh Ho <ho.thanhdanh9400@gmail.com>
2022-09-29 23:22:43 +0700

    29/9

--- 06 Networking/19 BaiThi Client Simple/Client.cs
+++ 06 Networking/19 BaiThi Client Simple/Client.cs
@@ -6,29 +6,74 @@ namespace BaiThi
 {
     internal class Client
     {
+        
+        static List<Menu> menuList = new List<Menu>();
+        static Socket client;
+        static String tableName="NoName";
         static void Main(string[] args)
         {
             // Kết nối tới server
             IPEndPoint iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
-            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             client.Connect(iep);
 
-            //Bắt đầu gửi nhận
-            byte[] bytes; string str;
+            menuList.Add(new Menu("Set Table Name", SetTableName));
+
             while (true)
             {
-                // Nhập String, encode String sang Byte và gửi
-                str = Console.ReadLine();                               //Nhập string
-                bytes = new byte[1024];                                 //Tạo byte mới
-                bytes = Encoding.UTF8.GetBytes(str);                    //Encode String vừa nhập sang Byte
-                client.Send(bytes, bytes.Length, SocketFlags.None);     //Gửi byte
-
-
-                // Nhận byte từ Server và Encode sang String
-                bytes = new byte[1024];                                 //Tạo byte mới
-                client.Receive(bytes);                                  //Nhận dữ liệu từ server vào byte     (Chờ server)
-                str = Encoding.UTF8.GetString(bytes);                   //Encode Byte sang String
-                Console.WriteLine("Server: {0}", str);                  //Print
+                GetAndDoAction();
+            }
+        }
+        static void GetAndDoAction() {
+            
+            int action = 0;
+            do
+            {
+                try
+                {
+                    PrintMenuList();
+                    Console.WriteLine("Choose: ");
+                    action = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("failed");
+                }
+            } while (action < 1 || action > menuList.Count);
+            menuList[action-1].voidName.DynamicInvoke();
+        }
+        static void Send(String str) {
+            byte[] bytes = new byte[1024];
+            bytes = Encoding.UTF8.GetBytes(str);
+            client.Send(bytes, bytes.Length, SocketFlags.None);
+        }
+        static String ReceivedString() {
+            byte[] bytes = new byte[1024];
+            client.Receive(bytes);
+            return Encoding.UTF8.GetString(bytes);
+            
+        }
+        static void PrintMenuList() {
+            Console.WriteLine("=========================");
+            Console.WriteLine("Table: "+tableName);
+            foreach (Menu menu in menuList) {
+                Console.WriteLine(menuList.IndexOf(menu) + 1+". "+menu.menuName);
+            }
+        }
+        static void SetTableName() {
+            Console.Write("New Table Name: ");
+            tableName = Console.ReadLine();
+            Send("SetTableName:" + tableName);
+            
+        }
+        internal class Menu
+        {
+            internal String menuName;
+            internal Delegate voidName;
+            public Menu(String _menuName, Delegate _voidName)
+            {
+                menuName = _menuName;
+                voidName = _voidName;
             }
         }
     }
--- 06 Networking/20 Server - Multiple Clients/Program.cs
+++ 06 Networking/20 Server - Multiple Clients/Program.cs
@@ -6,6 +6,19 @@ namespace _20_Server___Multiple_Clients
 {
     internal class Server
     {
+        internal class Table
+        {
+            public Socket socket;
+            public String name;
+            public Table(Socket _tableSocket, String _name) { 
+                socket = _tableSocket;
+                name = _name;
+                Console.WriteLine("New Table Connecting");
+                StartClient(socket);
+            }
+        }
+
+        static List<Table> tables = new List<Table>();
         static void Main(string[] args)
         {
             //Tạo kết nối và kết nối với client
@@ -14,18 +27,39 @@ namespace _20_Server___Multiple_Clients
             server.Bind(iep);
             server.Listen(99);
 
+            new Thread(() => {
+                Console.WriteLine("Ready");
+                while (true)
+                    tables.Add(new Table(server.Accept(), " "));
+            }).Start();
             
-            while(true) StartClient(server.Accept());
 
         }
         static void StartClient(Socket client) {
             new Thread(() => {
                 //Start
-                
+                SetTableName(client, "NoName");
                 while (true)
                 {
-                    Console.WriteLine("Client says: " + GetStringData(client));
-                    SendStringData(client, "Hello");
+
+                    String dataFromClient="";
+                    try { dataFromClient = GetStringData(client); } catch (SocketException) {
+                        Console.WriteLine("Table "+GetTable(client).name+" Closed");
+                        return; }
+                    Console.WriteLine("Table " + GetTable(client).name + " sent: \"" + dataFromClient+ "\"");
+                    String command = (dataFromClient.Split(":"))[0];
+
+                    switch (command) {
+
+                        case "SetTableName":
+                            SetTableName(client, (dataFromClient.Split(":"))[1]);
+                            Console.WriteLine("Table new name: "+ (dataFromClient.Split(":"))[1]);
+                            break;
+
+                        default:
+                            break;
+                    }
+
                 }
 
 
@@ -47,5 +81,25 @@ namespace _20_Server___Multiple_Clients
             bytes = Encoding.UTF8.GetBytes(str);
             client.Send(bytes, bytes.Length, SocketFlags.None);
         }
+        static void SetTableName(Socket _tableSocket,String _name) {
+            foreach (Table table in tables) {
+                if (table.socket == _tableSocket) { table.name = _name; break; }
+            }
+        }
+        static Table GetTable(String _name) {
+            foreach (Table table in tables) {
+                if (table.name.Equals(_name)) return table;
+            }
+            return null;
+        }
+        static Table GetTable(Socket _socket)
+        {
+            foreach (Table table in tables)
+            {
+                if (table.socket==_socket) return table;
+            }
+            return null;
+        }
+
     }
 }
